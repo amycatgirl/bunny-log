c3bb53ea180a9e50e6c3d495b5c9a5b7d62a6f95
Amelia <amy+git@amogus.cloud>
2026-06-01 07:41:41 -0400

    feat: permalinks
    silly
    
    Signed-off-by: Amelia <amy+git@amogus.cloud>

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-import { defineNavigationHook } from "#app/router"
+import { defineNavigationHook, navigate } from "#app/router"
+
+// https://npmx.dev/package-code/@atcute/tid/v/1.1.2/lib%2Findex.ts
+const TID_RE = /^[234567abcdefghij][234567abcdefghijklmnopqrstuvwxyz]{12}$/;
+const validateTID = (tid) => {
+    return tid.length === 13 && TID_RE.test(tid);
+};
 
 const DEFAULT_PREVIEW_HANDLE = "bunniesin.space";
 const DEFAULT_PREVIEW_DID = "did:plc:gijpvbkdbr56kazbdjhfvb3d";
@@ -46,12 +52,19 @@ function replacePlaceholders() {
     })
 }
 
+// do this asap
+replacePlaceholders();
+
 function toggleLoading() {
     const loader = document.getElementById('loader');
 
     loader.classList.toggle('hidden');
 }
 
+
+function extractRkeyFromPlainAtURI(uri) {
+    return uri.split("/").at(-1)
+}
 
 export function constructApiUrl(endpoint_nsid, options, api = DEFAULT_PREVIEW_DID_PDS) {
     const url = new URL(`${api}/xrpc/${endpoint_nsid}`);
@@ -76,20 +89,49 @@ async function fetchPostsFromPreviewDID(previous_cursor) {
 
     if (!res.ok) {
         console.error("[APP]", "failed to fetch latest logs:", res.statusText)
+        displayError("fetchPreviewList", res.statusText)
         return;
     }
 
     const {cursor, records} = await parseResponseBody(res)
     preview_cursor = cursor;
 
-    return records.map(record => record.value);
+    return records.map(record => ({ ...record.value, rkey: extractRkeyFromPlainAtURI(record.uri)}));
+}
+
+/** @param {string} rkey - Record key */
+async function fetchSinglePostFromPreviewDID(rkey) {
+    const res = await fetch(constructApiUrl("com.atproto.repo.getRecord", {
+        repo: DEFAULT_PREVIEW_DID,
+        collection: "space.bunniesin.micro.log",
+        rkey
+    }))
+
+    if (!res.ok) {
+        console.error("[APP]", "failed to fetch log:", res.statusText)
+        displayError("fetchPreviewList", res.statusText)
+        navigate("log-preview")
+        return;
+    }
+
+    const { value, uri } = await parseResponseBody(res);
+
+    return {...value, rkey: extractRkeyFromPlainAtURI(uri)}
 }
 
 function displayLog(record) {
+    if (record["$type"] !== "space.bunniesin.micro.log") throw new Error(`Invalid record type ${record["$type"]}`)
+    
     const logElement = document.createElement('div');
     logElement.classList.add('log');
     const rendered_log = record.content.split("\n\n").map(line => `<p>${line}</p>`).join("\n")
-    logElement.innerHTML = `${rendered_log}\n<time datetime=${record.createdAt}>${DATE_FORMATTER.format(new Date(record.createdAt))}</time>`;
+
+    const permalink = new URL(window.location);
+    permalink.searchParams.set("log", record.rkey);
+
+    logElement.innerHTML = `${rendered_log}\n
+<time datetime=${record.createdAt}>${DATE_FORMATTER.format(new Date(record.createdAt))}</time>
+<button class="show-hover" onclick="navigator.clipboard.writeText('${permalink.toString()}')">copy permalink</button>`;
 
     return logElement
 }
@@ -114,15 +156,6 @@ export async function fetchAndDisplayLatestLogs(cursor) {
 
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    replacePlaceholders();
-    fetchAndDisplayLatestLogs()
-})
-
-defineNavigationHook("log-preview", () => {
-    fetchAndDisplayLatestLogs()
-})
-
 export function displayError(context, message) {
     let errorKind;
     switch (context) {
@@ -136,6 +169,9 @@ export function displayError(context, message) {
             errorKind = "Error while creating micro.json:";
             break;
         case "fetchPreview":
+            errorKind = "Error while fetching log:";
+            break;
+        case "fetchPreviewList":
             errorKind = "Error while fetching latest logs:";
             break;
         default:
@@ -145,3 +181,26 @@ export function displayError(context, message) {
 
     alert(errorKind + " " + message)
 }
+
+async function handlePermalink() {
+    if (window.location.search) {
+        const tid = new URL(window.location).searchParams.get("log");
+
+        if (!validateTID(tid)) return;
+
+        const permalinkWrapper = document.querySelector("#log-permalink .wrapper");
+        const post = await fetchSinglePostFromPreviewDID(tid);
+
+        permalinkWrapper.replaceChildren(displayLog(post));
+        navigate("log-permalink")
+    }
+}
+
+defineNavigationHook("log-preview", () => {
+    fetchAndDisplayLatestLogs()
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchAndDisplayLatestLogs();
+    handlePermalink();
+})

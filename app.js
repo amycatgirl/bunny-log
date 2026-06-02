e4a7ec59d6f584d77c95c2ccffa75700996959fd
Amelia <amy+git@amogus.cloud>
2026-06-02 18:49:04 -0400

    chore: disable crosspost toggle if exceeds 300 chars
    Signed-off-by: Amelia <amy+git@amogus.cloud>

--- app.js
+++ app.js
@@ -316,3 +316,12 @@ document.addEventListener("DOMContentLoaded", () => {
     setupTypeaheadElements();
     fetchAndDisplayLatestLogs();
 })
+
+document.getElementById("log-content").addEventListener('input', (ev) => {
+    if (ev.currentTarget.value.length > 300) {
+        document.getElementById("do-crosspost").disabled = true;
+        document.getElementById("do-crosspost").checked = false;
+    } else {
+        document.getElementById("do-crosspost").disabled = false;
+    }
+})
